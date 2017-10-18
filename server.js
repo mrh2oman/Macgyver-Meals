347d78e548508f307195804ba11ae854274120ea
David Waterman <davidwaterman@Davids-MacBook-Air.local>
2017-10-17 22:49:24 -0500

    this

--- server.js
+++ server.js
@@ -10,10 +10,13 @@ var bodyParser = require("body-parser");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 // Requiring our models for syncing
 var db = require("./models");
+
+// Static directory
+app.use(express.static("public"));
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
@@ -21,8 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Static directory
-app.use(express.static("public"));
+
 
 // Routes
 // =============================================================
@@ -32,7 +34,7 @@ require("./routes/api-routes.js")(app);
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync({ force: false }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+    app.listen(PORT, function() {
+        console.log("App listening on PORT " + PORT);
+    });
 });
